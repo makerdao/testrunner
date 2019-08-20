dcd065050f9b05743cb4353b90d4ce1971449f2b
shkfnly <ashoka.finley@gmail.com>
2019-08-20 06:16:45 -0400

    Add babel confirm

---
+++ utilities/setup/babelConfirm.js
@@ -0,0 +1,5 @@
+require("babel-core").transform("code")
+
+var babelTest = require('./babelTest.js')
+
+babelTest('Testing', 'MakerDAO', 'Snowboarding', 'Pizza', 'Ice Cream')
---
+++ utilities/setup/babelTest.js
@@ -0,0 +1,13 @@
+const babelTest = (arg1, ...args) => {
+  let [arg2, arg3] = args
+
+  for(let arg of args) {
+    let arg2 = 'is the best'
+    console.log(`${arg} ${arg2}`)
+  }
+  console.log(`*************************`)
+  console.log(`Hello babel my old friend`)
+  console.log(`*************************`)
+}
+
+module.exports = babelTest
