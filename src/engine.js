00bdf48793d3ec3d5f16196f1545cd38c284449e
Ethan <ebennett19@gmail.com>
2019-09-10 03:40:42 -0600

    TESTRUN-7: refactor

--- src/engine.js
+++ src/engine.js
@@ -1,3 +1,5 @@
+import ErrorWithStack from "jest-util/build/ErrorWithStack";
+
 // this is the part that pulls in all the dependencies
 // so that actors and actions don't have to handle
 // their own imports
@@ -7,7 +9,7 @@ export default class Engine {
 
   }
 
-  run({ plans, actions, actors }) { // eslint-disable-line no-unused-vars
+  async run({ plans, actions, actors }) { // eslint-disable-line no-unused-vars
     // specify plans or actions/actors, not both
 
     // run the plans
@@ -15,39 +17,66 @@ export default class Engine {
     // initialize the users needed
     // run the actions in order, passing user as argument
 
-    let results = [];
+    let report = {
+      results: [],
+      success: true
+    }
     let items = actions ? [ ...actions ] : [];
-    let users = actors ? this._setUsers(actors) : {};
+    let users = actors ? this._importUsers(actors) : {};
 
     if (plans) {
-      plans.forEach(plan => {
-        const importedPlan = (require(`./plans/${plan}.js`)).default;
-        users = this._setUsers(importedPlan.actors);
-        importedPlan.actions.forEach(action => {
-          items.push(action);
-        });
-      });
+      const plan = this._importPlans(plans);
+      users = plan.users;
+      items = plan.items;
     }
 
-    items.forEach(async action => {
-      let result;
-      const importedAction = (require(`./actions/${action[1]}`)).default;
-      try {
-        result = await importedAction.operation(users[action[0]], action[0]);
-      } catch (err) {
-        console.error(err);
+    for (const item of items) {
+      if (report.success) {
+        const importedAction = (require(`./actions/${item[1]}`)).default;
+        const importedUser = users[item[0]]
+        try {
+          const result = await this._runAction(importedAction, importedUser, item[0]);
+          report.results.push(result);
+        } catch (err) {
+          report.success = false;
+          report.errors = {
+            action: item,
+            message: err.message,
+            errorIndex: 0 + report.results.length
+          };
+        }
       }
-      results.push(result);
-    });
-
-    return { results, success: true };
+    }
+    
+    return report;
   }
 
-  _setUsers(actors) {
+  _importUsers(actors) {
     let users = {};
     Object.keys(actors).forEach(name => {
       users[name] = (require(`./actors/${actors[name]}`)).default;
     });
     return users;
+  }
+
+  _importPlans(plans) {
+    let users = {};
+    let items = [];
+    plans.forEach(plan => {
+      const importedPlan = (require(`./plans/${plan}.js`)).default;
+      users = this._importUsers(importedPlan.actors);
+      importedPlan.actions.forEach(action => {
+        items.push(action);
+      });
+    });
+
+    return {
+      users: users,
+      items: items
+    }
+  }
+
+  async _runAction(action, actor, name) {
+    return await action.operation(actor, name);
   }
 }
