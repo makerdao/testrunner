f75be42543a626a07f1ee8757ff07847020e28d5
shkfnly <ashoka.finley@gmail.com>
2019-09-18 18:09:28 -0400

    TESTRUN-3: cherry-pick d758edf, fix conflict

--- test/placeholder.test.js
+++ test/placeholder.test.js
@@ -1,3 +1,21 @@
-test('basic setup', () => {
+import { Client, Event } from '@makerdao/testchain-client';
+
+const client = new Client(
+  'https://testchain-backendgatway.makerfoundation.com:4001'
+);
+
+beforeAll(async () => {
+  jest.setTimeout(30000);
+  await client.init();
+  await client.create({
+    clean_on_stop: false,
+    chainType: 'ganache',
+    block_mine_time: 0,
+    accounts: 3
+  });
+  console.log(await client.api.listAllChains());
+});
+
+test('basic setup', async () => {
   expect(1 + 1).toEqual(2);
 });
