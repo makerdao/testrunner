3cf84ee7d14a11c0da2f412b06324f4080ebf729
Ethan <ebennett19@gmail.com>
2019-09-20 19:08:58 -0600

    TESTRUN-3: import client in engine

--- src/testchain.js
+++ src/testchain.js
@@ -1,21 +1,25 @@
 import { Client } from '@makerdao/testchain-client';
 
 let cachedInstance;
+// We should make caching optional
 
-export default async function client() {
+export default function createClient() {
+  // We should probably accept an object of
+  // options here with the below as defaults
+
   if (cachedInstance) return cachedInstance;
 
   const client = new Client(
     'https://testchain-backendgatway.makerfoundation.com:4001'
   );
   client.init();
-  await client.create({
+  client.create({
     clean_on_stop: false,
     chainType: 'ganache',
     block_mine_time: 0,
     accounts: 3
   });
   cachedInstance = client;
-  console.log(await client.api.listAllChains());
+
   return client;
 }
