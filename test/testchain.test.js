9b9e28d8b7d489c1797807d8fe8083bbe9c30ee2
shkfnly <ashoka.finley@gmail.com>
2019-09-23 19:55:38 -0400

    update testchain test

--- test/testchain.test.js
+++ test/testchain.test.js
@@ -1,6 +1,7 @@
-// import createClient from '../src/testchain';
+import createClient from '../src/testchain';
 
 test('createClient returns a connected testchain client', async () => {
-  // const client = createClient();
-  // const networks = await client.api.listAllChains();
+  const client = createClient();
+  const networks = await client.api.listAllChains();
+  expect(networks.data[0].id).toBeDefined();
 });
