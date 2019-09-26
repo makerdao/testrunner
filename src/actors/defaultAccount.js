20840f5db4c8d2650fdd78cc17b587f9763bca7c
Lawrence Wang <l@lw.io>
2019-09-25 18:09:15 -0700

    add cautionary comment

--- src/actors/defaultAccount.js
+++ src/actors/defaultAccount.js
@@ -1,4 +1,6 @@
 export default async function(name, maker) {
+  // this is a bare-bones implementation; it won't work correctly with a maker
+  // instance that's been set up with multiple accounts
   return {
     address: maker.currentAccount()
   };
