a76f29f1d2391213ec71678c9ba082a210b7f333
Lawrence Wang <l@lw.io>
2019-09-25 17:50:27 -0700

    pass CLI options to engine
    at this point, self-tests should pass from CLI:
    ```
    ❯ node ./src/cli -p selfTestA
    { results: [ '0xa' ],
      success: true,
      completed: [ [ 'user1', 'checkUser' ] ] }
    ```

--- src/cli/core.js
+++ src/cli/core.js
@@ -1,6 +1,14 @@
 import Engine from '../engine';
+import omit from 'lodash/omit';
 
-export default function(options) {
-  const engine = new Engine();
-  console.log(options);
+export default async function(options) {
+  // TODO read the `config` arg to get additional configuration from a JSON file
+  options = {
+    ...omit(options, ['plan', 'config']),
+    plans: [options.plan]
+  };
+
+  const engine = new Engine(options);
+  const report = await engine.run();
+  console.log(report);
 }
