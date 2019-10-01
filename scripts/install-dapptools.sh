d152a61afdf92028a8a7ab89e6248fa90c63d4a6
Lawrence Wang <l@lw.io>
2019-10-01 10:59:52 -0700

    try michael's suggestion

--- scripts/install-dapptools.sh
+++ scripts/install-dapptools.sh
@@ -6,6 +6,9 @@ set -e
 export USER=circleci
 
 # sudo mkdir -m 0755 /nix && sudo chown circleci /nix
-# sudo mount -o bind /home/circleci/build/makerdao/dai.js/nix /nix
-curl https://dapp.tools/install | sh
-. /home/circleci/.nix-profile/etc/profile.d/nix.sh
+# sudo mount -o bind /home/circleci/project/nix /nix
+# curl https://dapp.tools/install | sh
+# . /home/circleci/.nix-profile/etc/profile.d/nix.sh
+
+git clone --recursive https://github.com/dapphub/dapptools $HOME/.dapp/dapptools
+nix-env -f $HOME/.dapp/dapptools -iA dapp seth solc hevm ethsign
