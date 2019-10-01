8f7eedf7c02dc6d9f55154af9c2485e69e8fd025
Lawrence Wang <l@lw.io>
2019-09-30 17:35:01 -0700

    will it blend?

---
+++ scripts/install-dapptools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# run this file with "source" or "." so it can affect the environment
+
+set -e
+
+export USER=circleci
+
+sudo mkdir -m 0755 /nix && sudo chown circleci /nix
+sudo mount -o bind /home/circleci/build/makerdao/dai.js/nix /nix
+curl https://dapp.tools/install | sh
+. /home/circleci/.nix-profile/etc/profile.d/nix.sh
