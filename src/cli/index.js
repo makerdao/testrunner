0ed628d6d3c5116261d31846d28609690e844c3b
Lawrence Wang <l@lw.io>
2019-10-02 08:58:53 -0700

    keystore arg is a directory

--- src/cli/index.js
+++ src/cli/index.js
@@ -2,7 +2,7 @@ const args = require('commander');
 args
   .option('-a, --address <hex>', 'address for defaultAccount (ETH_FROM)')
   .option('-c, --config <file>', 'config file name')
-  .option('-k, --keystore <file>', 'keystore for defaultAccount (ETH_KEYSTORE)')
+  .option('-k, --keystore <dir>', 'keystore for defaultAccount (ETH_KEYSTORE)')
   .option('--password', 'key password (ETH_PASSWORD)')
   .option('-p, --plan <name>', 'plan name')
   .option('-r, --rpc-accounts', 'use RPC accounts instead of keystore (ETH_RPC_ACCOUNTS)')
