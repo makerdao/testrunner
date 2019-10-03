57d1da5c4e08c1e91ef72384ba17d36e9aa7eb4f
Ethan <ebennett19@gmail.com>
2019-10-02 22:31:54 -0600

    TESTRUN-10: add operation for checking system-wide debt ceiling

--- src/actions/checkInitialConditions.js
+++ src/actions/checkInitialConditions.js
@@ -1,13 +1,6 @@
 export default {
-  before: async (actor, maker, currencies) => {
-    return await maker
-      .service('mcd:cdpManager')
-      .open('ETH-A', currencies.ETH(1), 2);
-  },
-  operation: (actor, maker, currencies) => {
-    console.log(maker.service('mcd:systemData'));
-    return maker.service('mcd:cdpType').debtCeiling;
-    // return maker.service('mcd:cdpType');
+  operation: async (actor, maker) => {
+    return await maker.service('mcd:systemData').getSystemWideDebtCeiling();
   },
   category: 'systemData'
 };
