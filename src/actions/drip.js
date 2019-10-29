ba8acd2e417ccb096d910e4b2e6a454cf28b1288
madumas <madumas@madumas.ca>
2019-10-29 17:02:08 -0400

    Basic cdp actions

---
+++ src/actions/drip.js
@@ -0,0 +1,15 @@
+import { stringToBytes } from '../../test/helpers/utils';
+
+export default {
+  // before and after can be omitted, but remain
+  // here as an example
+  before: () => {},
+  operation: async (user, maker) => {
+    return maker
+      .service('smartContract')
+      .getContract('MCD_JUG')
+      .drip(stringToBytes('ETH-A'));
+  },
+  after: () => {},
+  category: 'cdp'
+};
---
+++ test/cdpDraw.test.js
@@ -0,0 +1,14 @@
+import Engine from '../src/engine';
+
+test('cdp draw', async () => {
+  const engine = new Engine({
+    actors: { user1: 'selfTestUser' },
+    actions: [['user1', 'cdpLock'], ['user1', 'cdpDraw']],
+    url: 'http://localhost:2000'
+  });
+  const report = await engine.run();
+  const cdp = report.results[0];
+
+  expect(report.success).toBeTruthy();
+  expect(cdp.debtValue.toNumber()).toBeGreaterThanOrEqual(50);
+});
