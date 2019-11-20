0944014670b69eafb337439d49b9feb3542ffa8c
madumas <madumas@madumas.ca>
2019-11-20 10:50:02 -0500

    parametrize openCDP action

--- test/openCdp.test.js
+++ test/openCdp.test.js
@@ -1,4 +1,5 @@
 import Engine from '../src/engine';
+import { ETH } from '@makerdao/dai-plugin-mcd';
 
 test('basic cdp functions', async () => {
   const engine = new Engine({
@@ -11,5 +12,30 @@ test('basic cdp functions', async () => {
   expect(report.error).toBeFalsy();
   const cdp = report.results[0];
   expect(cdp.collateralValue.toNumber()).toEqual(150);
-  expect(cdp.debtValue.toNumber()).toEqual(1);
+  expect(cdp.debtValue.toNumber()).toEqual(20);
+});
+
+test('open cdp with parameters', async () => {
+  const engine = new Engine({
+    actors: { user1: 'selfTestUser' },
+    actions: [
+      [
+        'user1',
+        [
+          [
+            'openCdp',
+            undefined,
+            { ilk: 'ETH-A', collateral: ETH(0.5), dai: 20 }
+          ]
+        ]
+      ]
+    ],
+    url: 'http://localhost:2000'
+  });
+
+  const report = await engine.run();
+  expect(report.error).toBeFalsy();
+  const cdp = report.results[0];
+  expect(cdp.collateralValue.toNumber()).toEqual(75);
+  expect(cdp.debtValue.toNumber()).toEqual(20);
 });
