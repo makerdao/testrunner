a858716b7ceb075e2acbf30a95a6525cd3ae328a
Lawrence Wang <l@lw.io>
2019-11-27 16:25:17 -0800

    remove unnecessary test setup

--- test/systemData.test.js
+++ test/systemData.test.js
@@ -1,5 +1,4 @@
 import Engine from '../src/engine';
-import { setupCollateral, createMaker } from './helpers/utils';
 
 test('can get basic system data to verify deployment', async () => {
   const engine = new Engine({
@@ -7,9 +6,6 @@ test('can get basic system data to verify deployment', async () => {
     actions: [['user1', 'checkInitialConditions']],
     url: 'http://localhost:2000'
   });
-  const maker = await createMaker('http://localhost:2000');
-  await maker.authenticate();
-  await setupCollateral(maker, 'ETH-A', { price: 150, debtCeiling: 50 });
   const report = await engine.run();
   expect(report.results[0].debtCeiling).toBe(1000000);
   expect(report.results[0].baseRate).toBe(0);
